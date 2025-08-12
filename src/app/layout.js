d5ca54235b9cfe4480b43902358a5a9d4646c14f
Farjad <farjadabbas12@gmail.com>
2025-08-12 15:51:40 +0500

    Made the frontend of login page. Will add backend later

--- src/app/layout.js
+++ src/app/layout.js
@@ -22,7 +22,17 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <main
+          className="min-h-screen"
+          style={{
+            backgroundColor: "#111111",
+            backgroundImage: `radial-gradient(circle, rgba(255, 248, 222, 0.35) 0.8px, transparent 0.8px)`,
+            backgroundSize: "22px 22px",
+            backgroundPosition: "0 0",
+          }}
+        >
+          {children}
+        </main>
       </body>
     </html>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -5,17 +5,7 @@ import Hero from "@/components/Hero";
 export default function LandingPage() {
   return (
     <div className="film-grain-wrapper flex flex-col items-center justify-center space-y-4">
-      <main
-        className="min-h-screen"
-        style={{
-          backgroundColor: "#111111",
-          backgroundImage: `radial-gradient(circle, rgba(255, 248, 222, 0.35) 0.8px, transparent 0.8px)`,
-          backgroundSize: "22px 22px",
-          backgroundPosition: "0 0"
-        }}
-      >
-        <Hero />
-    </main>
+      <Hero />
     </div>
   );
 }
