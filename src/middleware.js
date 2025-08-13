59b5e95cff4827d5bbda33f1e25fdc917c14a987
Farjad <farjadabbas12@gmail.com>
2025-08-14 00:56:58 +0500

    .

--- src/middleware.js
+++ src/middleware.js
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import { verifyJWT } from "@/lib/session";
 
 const protectedRoutes = [""];
-const publicRoutes = ["/login", "/"];
+const publicRoutes = ["/login"];
 
 export async function middleware(req) {
   const path = req.nextUrl.pathname; //gives the path of the request
@@ -15,9 +15,9 @@ export async function middleware(req) {
     return NextResponse.redirect(new URL("/login", req.url));
   }
   //commenting this until we make more pages
-  //   if (isPublicRoute && payload) {
-  //     return NextResponse.redirect(new URL("/", req.url));
-  //   }
+  if (isPublicRoute && payload) {
+    return NextResponse.redirect(new URL("/", req.url));
+  }
   return NextResponse.next();
 }
 
