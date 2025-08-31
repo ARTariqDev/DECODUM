3625e763d0e22bfd7a35eb1e24248d7fba0dad57
Farjad <farjadabbas12@gmail.com>
2025-08-31 14:53:21 +0500

    Added score to the schema. Addded hash function to authentication and changed the seed data

--- src/lib/seed.js
+++ src/lib/seed.js
@@ -1,29 +1,34 @@
-// import LoginModel from "../models/login.js";
-// import mongoose from "mongoose";
-// const seedData = [
-//   { teamID: "team123", password: "password123" },
-//   { teamID: "team456", password: "password456" },
-// ];
+import LoginModel from "../models/login.js";
+import { hashPassword, checkPass } from "./hasher.js";
+import mongoose from "mongoose";
+const seedData = [
+  { teamID: "team123", password: "password123" },
+  { teamID: "team456", password: "password456" },
+];
 
-// async function seed() {
-//   try {
-//     await mongoose.connect(
-//       "mongodb+srv://artariqdev:Np12mgSbLXwyJ38Z@decodum.9k6wsgo.mongodb.net/?retryWrites=true&w=majority&appName=decodum"
-//     );
-//     console.log("Database connected successfully");
-//     // LoginModel.insertMany(seedData)
-//     //   .then(() => {
-//     //     console.log("Seed data inserted successfully");
-//     //   })
-//     //   .catch((error) => {
-//     //     console.error("Error inserting seed data:", error);
-//     //   });
-//     LoginModel.find({}).then((data) => {
-//       console.log("Seed Data:", data);
-//     });
-//   } catch (error) {
-//     console.error("Database connection failed:", error);
-//   }
-// }
+// const hashedData = await Promise.all(
+//   seedData.map(async (data) => {
+//     const hash = await hashPassword(data.password);
+//     return { ...data, password: hash };
+//   })
+// );
+
+async function seed() {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://artariqdev:Np12mgSbLXwyJ38Z@decodum.9k6wsgo.mongodb.net/?retryWrites=true&w=majority&appName=decodum"
+    );
+    console.log("Database connected successfully");
+
+    // LoginModel.deleteMany({}).then((data) => {
+    //   console.log("Deleted");
+    //   console.log(data);
+    // });
+    // await LoginModel.insertMany(hashedData);
+    LoginModel.find({}).then((data) => console.log(data));
+  } catch (error) {
+    console.error("Database connection failed:", error);
+  }
+}
 
 // seed();
--- src/models/login.js
+++ src/models/login.js
@@ -10,6 +10,11 @@ const loginSchema = new Schema({
     type: String,
     required: [true, "Password is required"],
   },
+  score: {
+    type: Number,
+    min: [0, "Score cannot be negative"],
+    default: 0,
+  },
 });
 
 export default mongoose.models.Login || mongoose.model("Login", loginSchema);
