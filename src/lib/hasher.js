463834ca823eea1e229a4bade01e1d2c70340b01
ARTariqDev <tariqmahmood.dmp@gmail.com>
2025-09-13 20:59:54 +0500

    debugged login issue (unsafe)

--- src/lib/hasher.js
+++ src/lib/hasher.js
@@ -1,20 +1,9 @@
-import bcrypt from "bcrypt";
 
-const saltRounds = 10;
-
+// Plain-text password comparison (not secure)
 export function hashPassword(plainPass) {
-  return bcrypt
-    .hash(plainPass, saltRounds)
-    .then((hash) => hash)
-    .catch((err) => console.error("error: ", err));
+  return plainPass;
 }
 
-export async function checkPass(plainPass, hash) {
-  try {
-    const result = await bcrypt.compare(plainPass, hash);
-    return result;
-  } catch (e) {
-    console.log("Error in comparing password");
-    console.error(err);
-  }
+export async function checkPass(plainPass, storedPass) {
+  return plainPass === storedPass;
 }
